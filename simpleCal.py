2796038540a338fd6d77b212f55767ad7ca6d997
Thomas <frankswatch@msn.com>
2019-04-30 20:20:06 +0800

    20190430

---
+++ simpleCal.py
@@ -0,0 +1,11 @@
+def add(i, j, k) :
+    return(i + j + k)
+
+def substract(i, j, k) :
+    return(i - j - k)
+
+def multipy(i, j, k) :
+    return(i * j * k)
+
+def divid(i, j, k) :
+    return(i / j / k)
